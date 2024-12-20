b0b335b592a999d3649fe3cd65483367a26c0aad
исп-22 <-22@LAB30-14>
2024-12-20 13:41:56 +0300

    Добавьте файлы проекта.

---
+++ practich9/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите элементы массива (разделяйте их пробелом): ");
+string n = Console.ReadLine();
+
+string[] y = n.Split(' ');
+int[] arr = new int[n.Length];
+
+for (int i = 0; i < y.Length; i++)
+{
+    arr[i] = int.Parse(y[i]);
+}
+
+int sum = 0;
+
+foreach (int num in arr)
+{
+    if (num % 7 == 0)
+    {
+        sum += num;
+    }
+}
+
+Console.WriteLine($"Сумма всех чисел, кратных 7: {sum}");
